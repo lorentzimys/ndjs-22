0e525b86a1ff5d7fb8642abff98381d236b514d5
lorentzimys <lorentzimys@yandex.ru>
2023-05-07 14:36:03 +0300

    Task 4: Finished headsAndTrails logic

--- scripts/headsAndTails.mjs
+++ scripts/headsAndTails.mjs
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 import { stdin } from 'node:process';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
+
+const argv = yargs(hideBin(process.argv))
+  .option('log', {
+    describe: 'Log results to file',
+    type: 'string',
+    demandOption: false,
+  }).argv;
+
 
 let selectedIndex = 0;
 
@@ -7,7 +18,30 @@ const guess = Math.floor(Math.random() * 2);
 
 const options = ['Орел', 'Решка'];
 
+const keyCode = {
+  up: '\u001b[A',
+  down: '\u001b[B',
+  enter: '\r',
+  ctrlC: '\u0003',
+}
+
+function logResults(logData, file, callback) {
+  fs.stat(file, function(err) {
+    if (err == null) {
+      fs.appendFileSync(file, logData, 'utf-8');
+    } else if (err.code === 'ENOENT') {
+      // file does not exist
+      fs.writeFileSync(file, logData, 'utf-8');
+    } else {
+      console.log('Could not log results, an error occured: ', err.code);
+    }
+
+    callback();
+  });
+}
+
 function start() {
+  // Включаем режим чтения потока в режиме raw
   stdin.setRawMode(true);
   
   // Переводим поток во flow режим
@@ -16,22 +50,51 @@ function start() {
   // Устанавливаем кодировку потока
   stdin.setEncoding( 'utf8' );
   
+  // Подписываемся на событие data
   stdin.on('data', (key) => {
-    if (key === '\u0003') { // Ctrl + C
+    let results = {
+      gameFinished: null,
+      guessed: null,
+      guess: null,
+    };
+
+    if (key === keyCode.ctrlC) {
+      results.gameFinished = false;
       process.exit();
-    } else if (key === '\u001b[A') { // Up arrow
+    } else if (key === keyCode.up) {
       selectedIndex = Math.max(0, selectedIndex - 1);
       ask();
-    } else if (key === '\u001b[B') { // Down arrow
+    } else if (key === keyCode.down) {
       selectedIndex = Math.min(1, selectedIndex + 1);
       ask();
-    } else if (key === '\r') { // Enter
+    } else if (key === keyCode.enter) {
       if (selectedIndex === guess) {
         console.log('Вы угадали!');
+
+        results = {
+          gameFinished: true,
+          guessed: true,
+          guess: options[selectedIndex],
+        }
+
       } else {
         console.log('Вы не угадали!');
+
+        results = {
+          gameFinished: true,
+          guessed: false,
+          guess: options[selectedIndex],
+        }
       }
-      process.exit();
+
+      if (argv.log) {
+        logResults(`${JSON.stringify(results)}\n`, argv.log, () => {
+          process.exit();
+        });
+      } else {
+        process.exit();
+      }
+
     }
   });
 
